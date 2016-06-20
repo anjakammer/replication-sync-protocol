b751d4d019000bd764eff4081f64acd1f30f9e10
Anja Kammer <post@anjakammer.de>
2016-06-20 17:36:38 +0200

    Update Application.java

--- src/Application.java
+++ src/Application.java
@@ -3,7 +3,7 @@ public class Application{
     public static void main(String[] args) {
 		ConcreteDBHandler Handler = new ConcreteDBHandler();
 		Handler.dosmth();
+		Handler.getUpdateSequence();
 		Handler.write();
-		Handler.dosmthelse();
 	}
 }

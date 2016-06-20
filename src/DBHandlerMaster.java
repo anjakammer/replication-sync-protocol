5b14b5c84d0fb456ec7c61e148ea5e062b6f59ca
Anja Kammer <post@anjakammer.de>
2016-06-20 17:35:33 +0200

    Update DBHandlerMaster.java

--- src/DBHandlerMaster.java
+++ src/DBHandlerMaster.java
@@ -1,10 +1,11 @@
-
 public abstract class DBHandlerMaster extends DBHandler{
 
- public void delete(){
-        System.out.println("Delete from DB");
-    } 
-    public void write(){
-        System.out.println("Write to DB");
-    } 
+ public void getUpdateSequence(){
+  System.out.println("returns transactions to update DB as Master");
+ }
+ 
+ abstract void write();
+ 
+ abstract void delete();
+ 
 }
